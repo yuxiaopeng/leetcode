2b7e5dfa57fd998b880e694b1afafafd21880c99
yuxiaopeng <uxiaopeng@gmail.com>
2023-05-09 12:57:09 +0800

    solution1 steps

--- algorithms/java/Java/src/mergeTwoSortedLists/Solution.java
+++ algorithms/java/Java/src/mergeTwoSortedLists/Solution.java
@@ -7,6 +7,22 @@ package mergeTwoSortedLists;
  * Choose the smaller head as the new head of the merged list.
  * Recursively merge the remaining elements of the list that contributed the smaller head with the entire other list.
  * Return the new merged list.
+ * Example1:
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * 
+ * step1:
+ * L1 = 1, L2 = 1, L1 = L2 => return L2, result = [1]
+ * step2:
+ * L1 = 1, L2 = 3, L1 < L2 => return L1, result = [1, 1]
+ * step3:
+ * L1 = 2, L2 = 3, L1 < L2 => return L1, result = [1, 1, 2]
+ * step4:
+ * L1 = 4, L2 = 3, L1 > L2 => return L2, result = [1, 1, 2, 3]
+ * step5:
+ * L1 = 4, L2 = 4, L1 = L2 => return L2, result = [1, 1, 2, 3, 4]
+ * step5:
+ * L1 = 4, L2 = null, L2 == null => return L1, result = [1, 1, 2, 3, 4, 4]
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
