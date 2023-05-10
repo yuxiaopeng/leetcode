8a5309cbeb3ca704bdfe73e0bd6f34f11621684e
yuxiaopeng <uxiaopeng@gmail.com>
2023-05-10 12:55:16 +0800

    merge two sorted array:solution1,append and sort

---
+++ algorithms/java/Java/src/mergeSortedArray/Solution.java
@@ -0,0 +1,20 @@
+package mergeSortedArray;
+
+import java.util.Arrays;
+
+class Solution {
+	/**
+	 * Time complexity: O((m+n)log(m+n))
+	 * Space complexity: O(log(m+n))
+	 * @param nums1
+	 * @param m
+	 * @param nums2
+	 * @param n
+	 */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i != n; ++i) {
+        	nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+}
